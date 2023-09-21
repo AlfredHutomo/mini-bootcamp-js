8f836779780461d086b263cda3053dec2f69f162
AlfredHutomo <alfredhutomo@gmail.com>
2023-09-21 19:01:43 +0700

    initial commit

---
+++ 1-data-types.js
@@ -0,0 +1,8 @@
+// var, let, const
+// number, string, boolean, null, undefined
+
+let age = 10;
+let fruit = "apple";
+let working = true;
+
+console.log(age, fruit, working);
---
+++ 3-functions.js
@@ -0,0 +1,3 @@
+// traditional function, parameters are passed in
+
+// arrow function, can be assigned as variable
