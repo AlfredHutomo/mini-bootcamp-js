164d1494ad0dfc2b67a63ee5ecde3ab98a5fc4cd
AlfredHutomo <alfredhutomo@gmail.com>
2023-09-22 10:19:06 +0700

    tambah operator

--- 2-operators.js
+++ 2-operators.js
@@ -32,3 +32,19 @@ console.log(false || false);
 console.log(true || false);
 console.log(false || true);
 console.log(true || true);
+
+// Assertive Operator akan return hasil dalam boolean bisa di gunakan dalam loop dan conditional
+
+// operator === dan == untuk membandingkan variable jika sama
+console.log(true === true); // true
+console.log(3 === "3"); //false
+console.log(3 == "3"); // true
+
+// operator !== dan != untuk membandingkan variable jika tidak sama
+console.log(false !== true); // true
+console.log(10 != "10"); // false
+console.log(10 !== "10"); // true
+
+// tips gunakan === atau !== untuk cek data dan tipe mereka agar lebih tepat
+
+// Operator assertive lainnya seperti >, >=, <, <= sama seperti di matematika
